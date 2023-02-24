1f175a2af1512b8f83391f22e229d49a1850d261
Ivan Klimov <klimov128@gmail.com>
2023-02-24 12:24:48 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryWrapper = document.querySelector('.gallery');
+
+function createGallary(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+         </a>`
+    )
+    .join('');
+}
+
+galleryWrapper.insertAdjacentHTML('afterbegin', createGallary(galleryItems));
+
+galleryWrapper.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  evt.preventDefault();
+
+  console.log(evt.target);
+  new SimpleLightbox('.gallery a');
+}
+
+// function showImageInModal() {
+//   new SimpleLightbox('.gallery a');
+// }
